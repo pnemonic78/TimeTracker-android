d8187ecbfc4787fe92ee5945737377ee203449a5
Moshe <moshe.waisberg@tikalk.com>
2017-06-20 11:48:24 +0300

    assert sample JSON.

--- WorkTracker/app/src/androidTest/java/com/tikalk/worktracker/model/ModelUnitTest.java
+++ WorkTracker/app/src/androidTest/java/com/tikalk/worktracker/model/ModelUnitTest.java
@@ -17,6 +17,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.lang.reflect.Modifier;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
@@ -41,6 +43,30 @@ public class ModelUnitTest {
 
         assertNotNull(records);
         assertEquals(2, records.length);
+
+        TimeRecord record = records[0];
+        assertNotNull(record);
+        assertEquals(100L, record.getId().longValue());
+        assertEquals(1, record.getVersion());
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        calendar.setTimeInMillis(0L);
+        calendar.set(2017, Calendar.JUNE, 1, 9, 00, 00);
+        assertEquals(calendar.getTime(), record.getStart());
+        calendar.set(2017, Calendar.JUNE, 1, 18, 00, 00);
+        assertEquals(calendar.getTime(), record.getFinish());
+        User user = record.getUser();
+        assertNotNull(user);
+        assertEquals(10L, user.getId().longValue());
+        assertEquals("test", user.getUsername());
+        assertEquals("test@tikalk.com", user.getEmail());
+        ProjectTask task = record.getTask();
+        assertNotNull(task);
+        assertEquals(101L, task.getId().longValue());
+        assertEquals("General", task.getName());
+        Project project = task.getProject();
+        assertNotNull(project);
+        assertEquals(1L, project.getId().longValue());
+        assertEquals("Tikal", project.getName());
     }
 
     private Reader openReader(String name) throws IOException {
