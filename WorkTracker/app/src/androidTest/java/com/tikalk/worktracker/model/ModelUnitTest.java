9e2525150a8f9bdbf136796e8c786e1a8fa0c872
Moshe <moshe.waisberg@tikalk.com>
2017-06-20 10:50:45 +0300

    test sample JSON.

--- WorkTracker/app/src/androidTest/java/com/tikalk/worktracker/model/ModelUnitTest.java
+++ WorkTracker/app/src/androidTest/java/com/tikalk/worktracker/model/ModelUnitTest.java
@@ -1,13 +1,23 @@
 package com.tikalk.worktracker.model;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+
+import com.google.gson.Gson;
+import com.tikalk.worktracker.time.model.TimeRecord;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * Test the data model.
@@ -17,10 +27,23 @@ import static junit.framework.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class ModelUnitTest {
     @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    public void time1() throws Exception {
+        Reader reader = openReader("time/time-1.json");
+        assertNotNull(reader);
 
-        assertEquals("com.tikalk.worktracker", appContext.getPackageName());
+        Gson gson = new Gson();
+        TimeRecord[] records = gson.fromJson(reader, TimeRecord[].class);
+        reader.close();
+
+        assertNotNull(records);
+        assertEquals(2, records.length);
+    }
+
+    private Reader openReader(String name) throws IOException {
+        Context context = InstrumentationRegistry.getContext();
+        AssetManager assets = context.getAssets();
+        InputStream in = assets.open(name);
+        assertNotNull(in);
+        return new InputStreamReader(in);
     }
 }
