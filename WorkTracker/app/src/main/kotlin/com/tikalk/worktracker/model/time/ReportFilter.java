da8a049e4ebe78b460ea61766eba27a98e0d8c28
Moshe <pnemonic@gmail.com>
2018-05-07 14:55:34 +0300

    migrate to kotlin

--- WorkTracker/app/src/main/kotlin/com/tikalk/worktracker/model/time/ReportFilter.java
+++ WorkTracker/app/src/main/kotlin/com/tikalk/worktracker/model/time/ReportFilter.java
@@ -29,7 +29,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.tikalk.worktracker.time.model;
+package com.tikalk.worktracker.model.time;
 
 import android.arch.persistence.room.Entity;
 import android.support.annotation.Nullable;
