fea90d546a5b3e506caeee463fbe4f808c9f6e59
M. Waisberg <pnemonic@gmail.com>
2019-09-15 14:16:06 +0300

    animated spinner

--- WorkTracker/app/src/main/kotlin/com/tikalk/worktracker/start/SplashActivity.kt
+++ WorkTracker/app/src/main/kotlin/com/tikalk/worktracker/start/SplashActivity.kt
@@ -31,35 +31,20 @@
  */
 package com.tikalk.worktracker.start
 
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.text.format.DateUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.tikalk.worktracker.R
 import com.tikalk.worktracker.time.TimeListActivity
-import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
     private val context: Context = this
 
-    private lateinit var rotate: ObjectAnimator
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        rotate = ObjectAnimator.ofInt(logo, "imageLevel", 0, 10000)
-        rotate.duration = DateUtils.SECOND_IN_MILLIS
-        rotate.repeatCount = ValueAnimator.INFINITE
-        rotate.start()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        rotate.cancel()
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
