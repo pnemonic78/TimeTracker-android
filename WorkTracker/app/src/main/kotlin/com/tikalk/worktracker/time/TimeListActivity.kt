ae1d5e21c5dae81cfdd0e56ed31ce29470897374
M. Waisberg <pnemonic@gmail.com>
2019-11-19 12:19:39 +0200

    add navigation

--- WorkTracker/app/src/main/kotlin/com/tikalk/worktracker/time/TimeListActivity.kt
+++ WorkTracker/app/src/main/kotlin/com/tikalk/worktracker/time/TimeListActivity.kt
@@ -35,31 +35,31 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.findNavController
 import com.tikalk.view.showAnimated
 import com.tikalk.worktracker.R
 import com.tikalk.worktracker.net.InternetActivity
-import com.tikalk.worktracker.preference.TimeSettingsFragment
 import kotlinx.android.synthetic.main.progress.*
 
 class TimeListActivity : InternetActivity() {
 
-    private lateinit var mainFragment: TimeListFragment
+//    private lateinit var mainFragment: TimeListFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Set up the form and list.
         setContentView(R.layout.activity_time_list)
-        val mainFragmentExisting = supportFragmentManager.findFragmentByTag(TAG_MAIN_FRAGMENT) as TimeListFragment?
-        if (mainFragmentExisting != null) {
-            mainFragment = mainFragmentExisting
-        } else {
-            mainFragment = TimeListFragment()
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.fragment_container, mainFragment, TAG_MAIN_FRAGMENT)
-                .commit()
-        }
+//        val mainFragmentExisting = supportFragmentManager.findFragmentByTag(TAG_MAIN_FRAGMENT) as TimeListFragment?
+//        if (mainFragmentExisting != null) {
+//            mainFragment = mainFragmentExisting
+//        } else {
+//            mainFragment = TimeListFragment()
+//            supportFragmentManager
+//                .beginTransaction()
+//                .replace(R.id.fragment_container, mainFragment, TAG_MAIN_FRAGMENT)
+//                .commit()
+//        }
 
         handleIntent(intent, savedInstanceState)
     }
@@ -88,42 +88,35 @@ class TimeListActivity : InternetActivity() {
     }
 
     private fun handleIntent(intent: Intent, savedInstanceState: Bundle? = null) {
-        if (savedInstanceState == null) {
-            mainFragment.arguments = intent.extras
-            mainFragment.run()
-
-            if (intent.action == ACTION_STOP) {
-                intent.action = null
-                mainFragment.stopTimer()
-            }
-        }
+//        if (savedInstanceState == null) {
+//            mainFragment.arguments = intent.extras
+//            mainFragment.run()
+//
+//            if (intent.action == ACTION_STOP) {
+//                intent.action = null
+//                mainFragment.stopTimer()
+//            }
+//        }
         intent.action = null
     }
 
     override fun onBackPressed() {
-        val settingsFragment = supportFragmentManager.findFragmentByTag(TAG_SETTINGS_FRAGMENT)
-        if (settingsFragment != null) {
-            supportFragmentManager.popBackStack()
-            return
-        }
-        if (mainFragment.onBackPressed()) {
-            return
-        }
+//        val settingsFragment = supportFragmentManager.findFragmentByTag(TAG_SETTINGS_FRAGMENT)
+//        if (settingsFragment != null) {
+//            supportFragmentManager.popBackStack()
+//            return
+//        }
+//        if (mainFragment.onBackPressed()) {
+//            return
+//        }
         super.onBackPressed()
     }
 
     private fun showSettings() {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container, TimeSettingsFragment(), TAG_SETTINGS_FRAGMENT)
-            .addToBackStack(TAG_SETTINGS_FRAGMENT)
-            .commit()
+        findNavController(R.id.nav_host_fragment).navigate(R.id.action_timeList_to_settings)
     }
 
     companion object {
         const val ACTION_STOP = TimeListFragment.ACTION_STOP
-
-        private const val TAG_MAIN_FRAGMENT = "main_list"
-        private const val TAG_SETTINGS_FRAGMENT = "settings"
     }
 }
