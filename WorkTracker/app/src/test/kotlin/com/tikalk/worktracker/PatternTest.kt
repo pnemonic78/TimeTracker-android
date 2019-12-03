cbf933141abd11274bd30e5b66ff316e63e20669
M. Waisberg <pnemonic@gmail.com>
2019-12-03 13:23:06 +0200

    test patterns

---
+++ WorkTracker/app/src/test/kotlin/com/tikalk/worktracker/PatternTest.kt
@@ -0,0 +1,108 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2019, Tikal Knowledge, Ltd.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * • Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * • Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * • Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.tikalk.worktracker
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.junit.Assert.*
+import org.junit.Test
+import java.io.InputStreamReader
+import java.util.regex.Pattern
+
+/**
+ * Pattern tests.
+ */
+class PatternTest {
+
+    private fun readResource(name: String): String {
+        val stream = this.javaClass.classLoader!!.getResourceAsStream(name)
+        assertNotNull(stream)
+        val reader = InputStreamReader(stream)
+        val text = reader.readText()
+        assertNotNull(text)
+        return text
+    }
+
+    @Test
+    fun parseTaskIds() {
+        val html = readResource("time_script_1.php")
+        assertTrue(html.isNotEmpty())
+        val doc: Document = Jsoup.parse(html)
+        assertNotNull(doc)
+
+        val tokenStart = "var task_ids = new Array();"
+        val tokenEnd = "// Prepare an array of task names."
+        val scriptText = findScript(doc, tokenStart, tokenEnd)
+        assertNotNull(scriptText)
+
+        if (scriptText.isNotEmpty()) {
+            val pattern = Pattern.compile("task_ids\\[(\\d+)\\] = \"(.+)\"")
+            val lines = scriptText.split(";")
+            for (line in lines) {
+                val matcher = pattern.matcher(line)
+                if (matcher.find()) {
+                    val projectId = matcher.group(1)!!.toLong()
+                    assertNotEquals(0, projectId)
+
+                    val taskIds: List<Long> = matcher.group(2)!!
+                        .split(",")
+                        .map { it.toLong() }
+
+                    assertTrue(taskIds.isNotEmpty())
+                }
+            }
+        }
+    }
+
+    protected fun findScript(doc: Document, tokenStart: String, tokenEnd: String): String {
+        val scripts = doc.select("script")
+        var scriptText: String
+        var indexStart: Int
+        var indexEnd: Int
+
+        for (script in scripts) {
+            scriptText = script.html()
+            indexStart = scriptText.indexOf(tokenStart)
+            if (indexStart >= 0) {
+                indexStart += tokenStart.length
+                indexEnd = scriptText.indexOf(tokenEnd, indexStart)
+                if (indexEnd < 0) {
+                    indexEnd = scriptText.length
+                }
+                return scriptText.substring(indexStart, indexEnd)
+            }
+        }
+
+        return ""
+    }
+}
