5d383aab8d8a6447201828cbe0ad8d8ac8555343
M. Waisberg <pnemonic@gmail.com>
2019-12-11 14:49:01 +0200

    sort spinner projects and task by name

--- WorkTracker/app/src/main/kotlin/com/tikalk/worktracker/time/TimeFormFragment.kt
+++ WorkTracker/app/src/main/kotlin/com/tikalk/worktracker/time/TimeFormFragment.kt
@@ -137,7 +137,7 @@ abstract class TimeFormFragment : InternetFragment(),
         }
 
         target.clear()
-        target.addAll(projects)
+        target.addAll(projects.sortedBy { it.name })
     }
 
     fun populateTasks(select: Element, target: MutableList<ProjectTask>) {
@@ -160,7 +160,7 @@ abstract class TimeFormFragment : InternetFragment(),
         }
 
         target.clear()
-        target.addAll(tasks)
+        target.addAll(tasks.sortedBy { it.name })
     }
 
     protected open fun findTaskIds(doc: Document): String? {
@@ -375,11 +375,11 @@ abstract class TimeFormFragment : InternetFragment(),
             tasksDb.addAll(projectWithTasks.tasks)
         }
         projects.clear()
-        projects.addAll(projectsDb)
+        projects.addAll(projectsDb.sortedBy { it.name })
         this.projectEmpty = projects.firstOrNull { it.isEmpty() } ?: projectEmpty
 
         tasks.clear()
-        tasks.addAll(tasksDb)
+        tasks.addAll(tasksDb.sortedBy { it.name })
         this.taskEmpty = tasks.firstOrNull { it.isEmpty() } ?: taskEmpty
     }
 
