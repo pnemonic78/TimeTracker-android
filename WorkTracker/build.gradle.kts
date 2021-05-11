bf7cbf11803737867e13f0983d1fc28076827926
M. Waisberg <pnemonic@gmail.com>
2021-05-11 20:17:30 +0300

    migrate to kts

---
+++ WorkTracker/build.gradle.kts
@@ -0,0 +1,24 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        jcenter()
+        google()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${BuildVersions.kotlin_version}")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${BuildVersions.nav_version}")
+    }
+}
+
+allprojects {
+    repositories {
+        jcenter()
+        google()
+    }
+}
+
+//task clean(type: Delete) {
+//    delete(rootProject.buildDir)
+//}
