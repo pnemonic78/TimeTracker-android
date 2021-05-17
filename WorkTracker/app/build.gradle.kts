a79e4c249b9af64cf0eb90473b8f0aac7eccba18
M. Waisberg <pnemonic@gmail.com>
2021-05-18 01:18:02 +0300

    fix lint - KtxExtensionAvailable

--- WorkTracker/app/build.gradle.kts
+++ WorkTracker/app/build.gradle.kts
@@ -83,7 +83,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.4.0-beta01")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.preference:preference:1.1.1")
+    implementation("androidx.preference:preference-ktx:1.1.1")
     implementation("androidx.security:security-crypto:1.1.0-alpha03")
 
     // Testing
