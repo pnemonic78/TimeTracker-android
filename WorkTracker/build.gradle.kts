7ae5e3853a83eee35c00b1b7821e2040dc493470
Moshe Waisberg <pnemonic@gmail.com>
2021-11-14 15:32:16 +0200

    app 3.13

--- WorkTracker/build.gradle.kts
+++ WorkTracker/build.gradle.kts
@@ -11,8 +11,8 @@ buildscript {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${BuildVersions.nav_version}")
 
         // Crashlytics
-        classpath("com.google.gms:google-services:4.3.8")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.6.1")
+        classpath("com.google.gms:google-services:4.3.10")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.0")
     }
 }
 
