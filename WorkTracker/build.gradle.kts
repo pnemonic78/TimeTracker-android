f40ca5199995fd5eff9fc367057ea982b9127a33
Moshe Waisberg <pnemonic@gmail.com>
2022-04-09 23:43:05 +0300

    latest dependencies

--- WorkTracker/build.gradle.kts
+++ WorkTracker/build.gradle.kts
@@ -6,13 +6,13 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.1")
+        classpath("com.android.tools.build:gradle:7.1.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${BuildVersions.kotlin_version}")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${BuildVersions.nav_version}")
 
         // Crashlytics
         classpath("com.google.gms:google-services:4.3.10")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.0")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
     }
 }
 
