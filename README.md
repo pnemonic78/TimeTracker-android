a6bd09e04f1b88da1892b48aabd24538f3abe77d
Moshe <moshe.waisberg@tikalk.com>
2023-01-03 08:47:59 +0200

    help screen

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
 # Work Tracker for Android by Tikal
 
 Track hours worked.
+
+## Tasks
+**It is important to remember to fill in hours as usual even on vacation, sick or reserve days, and to indicate the specific type of activity.**
+
+### Tikal – Consulting
+* Customer service: under the customer's name – according to the type of task
+
+### Tikal – Development
+* Customer service: under the customer's name – according to the type of task
+* Laboratory
+
+### Tikal – Army Service
+* Reserves
+
+### Tikal – Illness
+* Sickness
+
+### Tikal – Vacation
+* Vacation
+
+### Tikal – Personal Absence
+* Unusual absences
+  * Birth
+  * Wedding
+  * Death of a first degree relative
+
+### Tikal – Meeting
+* Company gatherings
+* Fun days
+* Statuses
+* Group meetings
+* Community meetings
+* Meetings with clients to select a task
+* Reception day in Tikal
+
+### Tikal – Training
+* Professional guidance and training
+* Development of training materials
+  * Work on courses
+  * Work on workshops
+  * Work on lectures
+* Courses
+* Workshops
+* Fuse Day
+* Tikal Hackathon
+* Work on a Tikal task
+
+### Tikal – General
+* Tikal time – work from Tikal offices between tasks
+
+### Tikal – HR
+* Work with human resources
+* Interviews for candidates
+* Conversations with employees (for group leaders)
+
+### Tikal – Sales
+* Work with sales
+* Lectures at clients
+* Presale meetings
+
+### Tikal – Transport
+* Travel between clients
---
+++ WorkTracker/app/src/main/java/com/tikalk/worktracker/help/HelpFragment.kt
@@ -0,0 +1,86 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2019, Tikal Knowledge, Ltd.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * • Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * • Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * • Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.tikalk.worktracker.help
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.MainThread
+import androidx.fragment.app.viewModels
+import com.tikalk.worktracker.databinding.FragmentComposeBinding
+import com.tikalk.worktracker.net.InternetFragment
+import timber.log.Timber
+
+class HelpFragment : InternetFragment() {
+
+    override val viewModel by viewModels<HelpViewModel>()
+
+    private var _binding: FragmentComposeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentComposeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.composeView.setContent {
+            HelpScreen()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menu.clear()
+    }
+
+    @MainThread
+    override fun run() {
+        Timber.i("run first=$firstRun")
+    }
+
+    override fun authenticate(submit: Boolean) = Unit
+}
